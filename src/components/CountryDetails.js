3c6ad68fde73395291aab3233919130668099e79
github username <github email address>
2019-01-13 10:47:16 -0500

    static list of wines on country page

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import {Link} from 'react-router-dom'
 import WineCard from './WineCard'
 
 const CountryDetails = (props) => {
+  console.log(props)
   return (
     <div>
       {props.country ? (
@@ -11,16 +13,22 @@ const CountryDetails = (props) => {
           <img alt="country" src={props.country.img_url} className="country-image"/>
           <p>{props.country.bio}</p>
 
-            <WineCard wine={props.wines.map(wine => wine.country.name === props.country.name)} />
-
+          <h2>Wines from {props.country.name}:</h2>
+          {props.wines.slice(0,5).map(w =>
+            <ul>
+              <Link to={`/uncorked/wines/${w.id}`}>
+                <li>{w.name}</li>
+              </Link>
+            </ul>
+          )}
         </React.Fragment>
 
-      ) : "loading..."}
-
+      ) : ("loading...") }
 
     </div>
   )
 }
+
 
 const mapStateToProps = state => {
   return {
